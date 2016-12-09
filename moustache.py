689d8c2ac50fd2a81ec91c9e815dbaf95ee6734c
Nicolas TROTE <nicolas.trote@gmail.com>
2016-12-09 10:18:22 -0500

    Update moustache.py

--- moustache.py
+++ moustache.py
@@ -1,3 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This program is demonstration of OpenCV library.
+Python implementation by: Nicolas TROTE
+"""
+import sys
+import cv2.cv as cv
+from optparse import OptionParser
+
+
+# Parameters for haar detection
+# From the API:
+# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned
+# for accurate yet slow object detection. For a faster operation on real video
+# images the settings are:
+# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING,
+# min_size=<minimum possible face size
+
+
+min_size = (20, 20)
+image_scale = 2
+haar_scale = 1.2
+min_neighbors = 2
+haar_flags = 0
+
+
+def detect_and_draw(img, cascade, mask):
+    # allocate temporary images
+    gray = cv.CreateImage((img.width,img.height), 8, 1)
+    small_img = cv.CreateImage((cv.Round(img.width / image_scale),
+                   cv.Round (img.height / image_scale)), 8, 1)
+
+
+    # convert color input image to grayscale
+    cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+
+
+    # scale input image for faster processing
+    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+
+
+    cv.EqualizeHist(small_img, small_img)
+
+
+    if(cascade):
+        t = cv.GetTickCount()
+        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
+                                     haar_scale, min_neighbors, haar_flags, min_size)
+        t = cv.GetTickCount() - t
+        print "detection time = %gms" % (t/(cv.GetTickFrequency()*1000.))
+        if faces:
             for ((x, y, w, h), n) in faces:
                 
                 # Affichage du carré de recherche
